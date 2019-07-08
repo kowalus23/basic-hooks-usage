6e039a60ecd0d1a88faafdfa4ec0017e31df8527
kowalus23 <kowalus23@gmail.com>
2019-07-08 20:22:12 +0200

    built App and ResourceList based on hooks + connected with json placeholder through axios

---
+++ src/components/ResourceList.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const ResourceList = ({resource}) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResource = async () => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+
+    setResources(response.data);
+  };
+
+
+  useEffect(() => {
+      fetchResource(resource);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [resource]
+  );
+
+  return (
+    <div>
+      {resources.length}
+    </div>
+  );
+};
+
+export default ResourceList;
